bf27ef56b36ada79ed595adb7c70dc40da972cf7
B1900812 <99552857+B1900812@users.noreply.github.com>
2023-10-05 21:54:49 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,11 @@
+<!-- register.php -->
+
+<form method="post">
+    <h2>Register Merchant Account</h2>
+    <!-- Form fields for merchant registration go here -->
+    <input type="text" name="merchant_name" placeholder="Merchant Name" required>
+    <input type="text" name="contact_number" placeholder="Contact Number" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <textarea name="company_description" placeholder="Company Description" required></textarea>
+    <input type="submit" value="Submit">
+</form>
